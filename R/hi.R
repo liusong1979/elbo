d8bcec2567f6744bcbf9ea2c071f9addcc6376a6
Bios <bios@Bios.local>
2016-10-05 17:30:28 -0400

    check input to hi()

--- R/hi.R
+++ R/hi.R
@@ -4,21 +4,25 @@
 #'
 #'@param who character(1) The name of the person who you wish to start
 #'      a conversation with,
-#'  
+#'
 #'@return character(1) A kine if text to be used when starting conversations.
-#' 
+#'
 #'@examples
 #'  hi("Song Liu")
 #'
-#'@importFrom stat lm 
-#'  
+#'
 #'@export
 
 hi <- function(who){
+    stopifnot(
+      is.character(who),
+      length(who) == 1,
+      !is.na(who)
+    )
     paste("hello", shout(who), "you have", nchar(who), "letters in your name!")
 }
 
 shout <-function(who){
   toupper(who)
 }
-  
+

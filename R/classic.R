bf1b08451750a9c9b929bdb74cc31217cac4caad
Bios <bios@Bios.local>
2016-10-07 16:47:59 -0400

    implement inout_classic()
    - input two files and merge into a single data.frame

---
+++ R/classic.R
@@ -0,0 +1,30 @@
+#' Classic R approaches to data analysis
+#'
+#' The functions here ... data management and analysis
+#'
+#' @details There are two files. They are from a micorarray experiments.
+#' The 1st file, All-phenotype.csv descrbes the 128 samples. The 2nd file,
+#' is the exeriment data file
+#'
+#' @param data_file character(1) The path to the pheno data file.
+#'
+#' @param exprs_file character(1) The path to the expression file.
+#'
+#' @return A data.frame containing samples as rows and phenotype data and
+#' expression data as columns.
+#'
+#' @export
+
+input_classic <- function(data_file, exprs_file){
+  pdata <-read.csv(pdata_file, row.names = 1, check.names = FALSE)
+  exprs = read.csv(exprs_file, row.names = 1, check.names = FALSE)
+
+  all = merge(pdata, t(exprs), by=0)
+  return(all)
+
+
+  ## return a data.frame
+
+
+
+}

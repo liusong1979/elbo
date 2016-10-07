4ee35aed1b29654ce9431a5449cce9491f32c00a
Bios <bios@Bios.local>
2016-10-07 17:54:03 -0400

    implement 'input_tidy'
    - represent long data as a tidy

---
+++ R/tidy.R
@@ -0,0 +1,29 @@
+#' represent data in a ‘tidy’ format
+#'
+#' ‘Tidy’ data takes a “long” view of data, where each variable is a column.
+#'
+#' @details this approach popularized by Hadley Wickham; see the vignette
+#' in the package 'dplyr'
+#'
+#' @param pdata_file	character(1) The path to the pheno data file.
+#'
+#' @param exprs_file	character(1) The path to the expression file.
+#'
+#' @return A data.frame
+#'
+#' @importFrom reshape2 melt
+#' @importFrom dplyr tbl_df
+#' @importFrom magrittr %>%
+#'
+#' @export
+
+input_tidy <- function(pdata_file, exprs_file){
+  classic<-input_classic(pdata_file, exprs_file)
+  melt(classic, id.vars=1:22, variable.name ="probeset",
+       value.name="exprs" ) %>% tbl_df
+}
+
+# tidy = input_tidy(pdata_file, exprs_file)
+# probeset_mean<-tidy %>% group_by(probeset) %>% summarize(mean=mean(exprs))
+# probeset_mean %>% .[["mean"]] %>% density() %>% plot()
+
